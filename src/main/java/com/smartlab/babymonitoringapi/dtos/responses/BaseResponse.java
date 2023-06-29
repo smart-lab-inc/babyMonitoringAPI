e0fd814120ea843b062a75b7f9e3a18d080382dc
Fernando Guerrero <devrrior@gmail.com>
2023-06-29 10:31:04 -0600

    Connection to Mongo successfully

---
+++ src/main/java/com/smartlab/babymonitoringapi/dtos/responses/BaseResponse.java
@@ -0,0 +1,26 @@
+package com.smartlab.babymonitoringapi.dtos.responses;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Builder
+@Getter @Setter
+public class BaseResponse {
+
+    private HttpStatus status;
+
+    private Integer statusCode;
+
+    private String message;
+
+    private Boolean success;
+
+    private Object data;
+
+    public ResponseEntity<BaseResponse> apply() {
+        return new ResponseEntity<>(this, status);
+    }
+}
--- src/main/java/com/smartlab/babymonitoringapi/services/ISensorDataService.java
+++ src/main/java/com/smartlab/babymonitoringapi/services/ISensorDataService.java
@@ -1,6 +1,7 @@
 package com.smartlab.babymonitoringapi.services;
 
 import com.smartlab.babymonitoringapi.dtos.requests.CreateSensorData;
+import com.smartlab.babymonitoringapi.dtos.responses.BaseResponse;
 import com.smartlab.babymonitoringapi.persistance.mongo.documents.SensorData;
 
 import java.util.List;
@@ -8,5 +9,5 @@ import java.util.List;
 public interface ISensorDataService {
     com.smartlab.babymonitoringapi.persistance.mongo.documents.SensorData create(CreateSensorData createSensorData);
 
-    List<SensorData> createMany (List<CreateSensorData> createSensorData);
+    BaseResponse createMany (List<CreateSensorData> createSensorData);
 }
