2e174a275ad88945c6693043fa00e8106f30658f
Mauricio Cruz <92227312+mauriciocruzp@users.noreply.github.com>
2023-07-03 19:37:25 -0500

    BM-38: Add Swagger (#3)
    * Add springfox dependencies
    
    * Add springfox configuration
    
    * Change springfox configuration
    
    * BM-38: Fix Swagger implentation
    
    ---------
    
    Co-authored-by: Mauricio Cruz <mawcp7@gmail.com>
    Co-authored-by: Fernando Guerrero <devrrior@gmail.com>

---
+++ src/main/java/com/smartlab/babymonitoringapi/configuration/SwaggerConfiguration.java
@@ -0,0 +1,35 @@
+package com.smartlab.babymonitoringapi.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "JWT Token";
+        String moduleName = "Baby Monitoring";
+        final String apiTitle = String.format("%s API", StringUtils.capitalize(moduleName));
+        String apiVersion = "v1.0";
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
+                .info(new Info().title(apiTitle).version(apiVersion));
+    }
+}
