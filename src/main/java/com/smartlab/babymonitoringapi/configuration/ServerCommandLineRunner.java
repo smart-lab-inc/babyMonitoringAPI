60d8fd1c723cfd0155ac8d94dd00d09cbd381011
Fernando Guerrero <devrrior@gmail.com>
2023-07-06 09:44:52 -0600

    BM-46: Add Netty SocketIO

---
+++ src/main/java/com/smartlab/babymonitoringapi/configuration/ServerCommandLineRunner.java
@@ -0,0 +1,21 @@
+package com.smartlab.babymonitoringapi.configuration;
+
+import com.corundumstudio.socketio.SocketIOServer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class ServerCommandLineRunner implements CommandLineRunner {
+
+    @Autowired
+    private SocketIOServer server;
+
+    @Override
+    public void run(String... args) throws Exception {
+        server.start();
+    }
+}
