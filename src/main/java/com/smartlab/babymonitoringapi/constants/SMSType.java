f9ea1f339e8b1d36b1b66c3272f7b9288985bedb
Fernando Guerrero <devrrior@gmail.com>
2023-07-13 00:33:11 -0600

    BM-49: Replace manual getter instead lombok getter

--- src/main/java/com/smartlab/babymonitoringapi/constants/SMSType.java
+++ src/main/java/com/smartlab/babymonitoringapi/constants/SMSType.java
@@ -1,5 +1,8 @@
 package com.smartlab.babymonitoringapi.constants;
 
+import lombok.Getter;
+
+@Getter
 public enum SMSType {
     TRANSACTIONAL("Transactional"),
     PROMOTIONAL("Promotional");
@@ -8,9 +11,5 @@ public enum SMSType {
 
     SMSType(String value) {
         this.value = value;
-    }
-
-    public String getValue() {
-        return value;
     }
 }
